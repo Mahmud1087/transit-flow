f9bea6cac19572ccccd6e33cc12a977b6ae30721
Mahmud1087 <abdulazeezm578@gmail.com>
2024-01-30 02:50:23 +0100

    done with the Footer section mobile responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <section className='border-solid border-t-[1px] border-[#4e5683] bg-prim-2'>
-      <div className='flex justify-between items-center py-5 sm:w-[52rem] sm:mx-auto'>
-        <p className='text-[12px] font-normal font-[Krub] text-white ml-8'>
+    <section className='border-solid border-t-[1px] border-[#4e5683] bg-prim-2 px-6 sm:px-0'>
+      <div className='flex justify-between items-center flex-col-reverse py-5 w-full gap-5 sm:gap-0 sm:w-[52rem] sm:mx-auto sm:flex-row'>
+        <p className='text-[12px] font-normal font-[Krub] text-white text-center sm:text-start sm:ml-8'>
           Copyright © TransitFlow | Designed by VictorFlow - Powered by Webflow.
         </p>
         <div className='flex gap-4'>
--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -17,14 +17,14 @@ export default function SubFooter() {
 
   return (
     <section className='flex flex-col'>
-      <img src={img} alt='Image' />
+      <img src={img} alt='Image' className='h-48 sm:h-full' />
 
       <footer className='h-[4rem] bg-[#161f4ff0]'>
-        <div className='flex gap-8 sm:w-[52rem] h-full sm:mx-auto'>
-          <div className='flex items-center justify-center px-3 pr-24 bg-prim-2 h-full'>
+        <div className='flex gap-8 w-full flex-col h-full sm:flex-row sm:w-[52rem] sm:mx-auto'>
+          <div className='flex items-center justify-center px-3 py-8 bg-prim-2 h-full sm:pr-24 sm:py-0'>
             <img src={logo} alt='Transit logo' className='w-[10rem] px-5' />
           </div>
-          <div className='flex items-center gap-32'>
+          <div className='hidden sm:flex flex-col items-center gap-10 sm:gap-32 sm:flex-row'>
             <a
               href='#pages'
               className='text-[#e8e8e8] text-[1rem] font-medium cursor-pointer font-[Rubik]'
@@ -47,21 +47,21 @@ export default function SubFooter() {
         </div>
       </footer>
 
-      <article className='bg-prim-2 pb-10'>
-        <div className='flex gap-8 w-[52rem] mx-auto'>
+      <article className='bg-prim-2 pb-10 w-full px-6'>
+        <div className='flex flex-col gap-8 sm:w-[52rem] sm:mx-auto sm:flex-row'>
           <div className='flex flex-col'>
-            <p className='font-normal text-[#e8e8e8] font-[Krub] text-[11px] leading-5 pl-8 py-5 w-[16.5rem]'>
+            <p className='font-normal text-[#e8e8e8] font-[Krub] text-[11px] leading-5 py-5 sm:w-[16.5rem] sm:pl-8 '>
               Leverage agile frameworks to provide a robust synopsis for
               strategy collaborative thinking to further the overall value
               proposition.
             </p>
-            <div className='flex flex-col gap-4 pl-8'>
+            <div className='flex flex-row gap-4 sm:flex-col sm:pl-8'>
               {data.map((item) => {
                 const { id, text, image, other } = item;
 
                 return (
                   <div key={id} className='flex items-center gap-3'>
-                    <img src={image} alt='icon' className='w-10' />
+                    <img src={image} alt='icon' className='w-8 sm:w-10' />
                     <div className='flex flex-col'>
                       <p className='text-[#e8e8e8] text-[10px] font-[Krub]'>
                         {text}
@@ -76,8 +76,8 @@ export default function SubFooter() {
             </div>
           </div>
 
-          <div className='flex py-3 justify-center gap-24'>
-            <ul className='ml-1 w-[5rem]'>
+          <div className='flex py-3 gap-24 sm:justify-center'>
+            <ul className='ml-1 sm:w-[5rem]'>
               <li>
                 <a
                   href='#about'
@@ -164,13 +164,13 @@ export default function SubFooter() {
             </ul>
           </div>
 
-          <div className='py-5 ml-16 flex flex-col gap-3'>
+          <div className='py-5 flex flex-col gap-3 sm:ml-16 '>
             <input
               type='text'
               placeholder='Enroll here'
               className='text-[12px] p-4 py-2 placeholder-[#999] bg-transparent border border-solid border-[#4e5683] outline-none text-[#e8e8e8]'
             />
-            <div className='flex gap-4 w-full justify-center items-center'>
+            <div className='flex w-full items-center gap-6 sm:justify-center sm:gap-4 '>
               <button
                 type='submit'
                 className='text-prim-2 bg-gradient-to-r from-grad-1 via-grad-2 to-grad-3 cursor-pointer w-fit py-2 px-3 text-[12px] font-medium'

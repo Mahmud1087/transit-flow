f9f590ac6d03dfd2c415c63f5742abd5df09b136
Mahmud1087 <abdulazeezm578@gmail.com>
2024-01-30 02:02:25 +0100

    done with the Contact section mobile responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,29 +34,29 @@ export default function Contact() {
   return (
     <section
       id='contact'
-      className='flex flex-col gap-5 justify-center items-center py-28 bg-prim-2'
+      className='flex flex-col gap-5 justify-center items-center bg-prim-2 px-6 py-14 sm:px-0 sm:py-28 '
     >
-      <article className='flex flex-col gap-3 sm:w-[52rem]'>
+      <article className='flex flex-col gap-8 sm:gap-3 sm:w-[52rem]'>
         <Title bg='titleBg-3' text='white'>
           Contact
         </Title>
-        <section className='flex gap-16'>
-          <div className='w-[38%] flex flex-col gap-3'>
+        <section className='flex flex-col gap-16 sm:flex-row'>
+          <div className='flex w-full flex-col gap-6 sm:gap-3 sm:w-[38%] '>
             <h1 className='font-[Rubik] font-medium text-white text-[1.8rem] leading-6'>
               Get in touch with us
             </h1>
-            <p className='text-white leading-6 font-[Krub] text-[13px]'>
+            <p className='text-white leading-8 font-[Krub] sm:leading-6 sm:text-[13px]'>
               Leverage agile frameworks to provide a robust synopsis for
               strategy foster collaborative thinking to further the overall
               value.
             </p>
-            <div className='flex flex-col gap-6 mt-5'>
+            <div className='grid grid-cols-2 gap-6 sm:mt-5 sm:flex sm:flex-col'>
               {data.map((item) => {
                 const { id, text, image, other } = item;
 
                 return (
                   <div key={id} className='flex items-center gap-3'>
-                    <img src={image} alt='icon' className='w-12' />
+                    <img src={image} alt='icon' className=' w-9 sm:w-12' />
                     <div className='flex flex-col'>
                       <p className='text-white text-[11px] font-[Krub]'>
                         {text}
@@ -71,7 +71,7 @@ export default function Contact() {
             </div>
           </div>
 
-          <div className='flex flex-col w-[62%] gap-5'>
+          <div className='w-full flex flex-col gap-5 sm:w-[62%] '>
             <div className='grid grid-cols-2 gap-5'>
               <input
                 type='text'
@@ -106,14 +106,14 @@ export default function Contact() {
 
             <button
               type='submit'
-              className='text-prim-2 bg-gradient-to-r from-grad-1 via-grad-2 to-grad-3 cursor-pointer w-fit py-2 px-5 text-[12.5px] font-medium'
+              className='w-full text-prim-2 text-sm bg-gradient-to-r from-grad-1 via-grad-2 to-grad-3 cursor-pointer py-4 px-5 font-bold sm:font-medium sm:py-2 sm:text-[12.5px] '
             >
               Submit Message
             </button>
           </div>
         </section>
 
-        <div className='mt-12 grid grid-cols-4 w-full'>
+        <div className='w-full mt-12 grid grid-cols-2 px-3 sm:px-0 sm:grid-cols-4 '>
           {data2.map((item, index) => {
             const { image, alt } = item;
 
